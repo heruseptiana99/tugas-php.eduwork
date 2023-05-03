857039fd4f807f5a98ddde4974dda43d6d58753f
heruseptiana99 <heruseptiana99@gmail.com>
2023-05-03 20:50:29 +0700

    praktek 3

---
+++ praktek-individu/praktek3.php
@@ -0,0 +1,10 @@
+<?php
+
+    $nama = array("Pelita", "Najmina", "Anita", "Bayu", "Nasa", "Rahma");
+
+    for ($i=0; $i < count($nama); $i++) { 
+        echo "Nama : " . $nama[$i];
+        echo "<br>";
+    }
+
+?>
